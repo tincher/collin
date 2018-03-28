d939e3df96d119ad4f3fd03f1a3afd5bfbf6ce39
tincher <joel.ewig@gmail.com>
2018-03-29 01:51:09 +0200

    mongodb added; "/add" init

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
-const express = require('express');
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+
+MongoClient.connect(url, (err, db) => {
+    if (err) throw err;
+    var collindb = db.db("collindb").collection("collin");
+    var myobj = {
+        name: "asldfj",
+        wasandres: "asdölfjasdlk"
+    };
+    collindb.insertOne(myobj, (err, res) => {
+        if (err) throw err;
+        console.log("Inserted succesfully");
+    });
+    db.close();
+});
+
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 app.get('/test', (req, res) => {
     res.send("test successfull");
 });
+
+app.post('/add', (req, res) => {
+    console.log(req.body);
+    insertOneInDB(req.body);
+    res.send("hat klappt")
+});
+
+function insertOneInDB(doc) {
+    MongoClient.connect(url, (err, db) => {
+        if (err) throw err;
+        var collindb = db.db("collindb").collection("collin");
+        collindb.insertOne(doc, (err, res) => {
+            if (err) throw err;
+            console.log("Inserted succesfully");
+        });
+        db.close();
+    });
+}
